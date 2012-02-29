0aa76f0cf84d34be6c66fb0b3e2a7074389502a2
Michael Arestad <marestad@gmail.com>
2012-02-29 00:56:31 -0700

    Several minor changes.
    Made a few minor changes and updated readability. I also switched to
    single-line comments to be stripped from style.css. I changed the links
    on the 404 page and footer.

--- 404.php
+++ 404.php
@@ -4,6 +4,8 @@
 
     <p>Unfortunately, the page at <code><?php echo current_url(); ?></code> 
     can't be found, but don't give up hope yet! You can always try going back to 
-    the homepage, or searching.</p>
+    the <a href="<?php echo base_url(); ?>">homepage</a>, or
+    <!-- Need to make 'searching' activate search -->
+    searching.</p>
 </section>
 
